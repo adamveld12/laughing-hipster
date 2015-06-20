3df045d6875f6029b96f17211f5787bb0d81cde0
Adam Veldhousen <adam.veldhousen@mtmrecognition.com>
2015-06-20 13:31:00 -0500

    updated bootstrap to work in a better way

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,45 @@
+#!/bin/bash
+IFS='\n\t'
+set -euo pipefail
+
 source=$(pwd)
-bkup=~/dotfile_bkup
+bkup=~/home_bkup/
 dest=~
 
 
-mkdir -p $bkup
-echo "making a backup of old dotfiles into ${bkup}"
-ls -A ${dest} | grep ^\\..* | xargs -I file cp ~/file ${bkup}/file
+echo "making a backup of old dotfiles into ${bkup}..."
+rm -rf ${bkup}
+mkdir -p ${bkup}
 
-echo "loading modules"
+IFS=$'\n'
+for file in $(ls -lA "${source}" | grep "^-" | awk '{print $9}'); do
+  if [ -f "${dest}/${file}" ]; then
+    cp -v "${dest}/${file}" "${bkup}/${file}"
+  fi
+done
+
+echo ""
+echo "loading git modules..."
 git submodule init
 git submodule update
 
-echo "linking dotfiles from ${source} into ${dest}"
-cd ${dest}
-ls -A ${source} | xargs -I file cp --symbolic-link -r ${source}/file ${dest}
 
-rm -rf ~/.git
-rm -rf ~/bootstrap.sh
+echo "copying directories..."
+cp -rs ${source}/tools ${dest}/tools
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.25.4/install.sh | bash
+pushd ${dest} 2>&1 /dev/null
+echo "linking dotfiles from ${source} into ${dest}..."
+ls -lA "${source}" | grep "^-" | awk '{print $9}' | xargs -I file ln -vfs "${source}/file" "${dest}/file"
 
-source ~/.profile
+#set +u
+#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.25.4/install.sh | bash 
+#set -u
+
+echo "cleaning up..."
+# cleanup
+rm -rf ${dest}/bootstrap.sh
+rm -rf ${dest}/.git
+
+popd 2>&1 /dev/null
+
+echo "done!"
