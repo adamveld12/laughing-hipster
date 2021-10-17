2166fdd1dc29ea5050c446e2409978a5ab3e9b3b
Adam Veldhousen <adamveld12@gmail.com>
2021-10-16 22:44:15 -0500

    test: don't use ruby on install run

--- plugins/220-ruby.sh
+++ plugins/220-ruby.sh
@@ -7,13 +7,10 @@ if ! [[ -d "${RVM_HOME}" ]]; then
     command curl -sSL https://rvm.io/pkuczynski.asc | gpg2 --import -;
 
 	curl -sSL https://get.rvm.io | bash -s stable --ruby;
-	source ${RVM_HOME}/scripts/rvm;
-	rvm use ruby-3.0.0;
-	source ${RVM_HOME}/scripts/completion;
-else
-	echo "rvm_silence_path_mismatch_check_flag=1" > ~/.rvmrc;
-	source ${RVM_HOME}/scripts/rvm;
-	rvm use ruby-3.0.0 > /dev/null;
-	source ${RVM_HOME}/scripts/completion;
 fi
+
+echo "rvm_silence_path_mismatch_check_flag=1" > ~/.rvmrc;
+[[ -s ${RVM_HOME}/scripts/rvm ]] && source ${RVM_HOME}/scripts/rvm;
+[[ -s ${RVM_HOME}/scripts/completion ]] && source ${RVM_HOME}/scripts/completion;
+# ! [[ -z "$(which rvm)" ]] && rvm use ruby-3.0.0 > /dev/null;
 
