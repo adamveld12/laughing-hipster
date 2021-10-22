4bf7f9db658f949e2523e2b4139d569612b625c5
Adam Veldhousen <adamveld12@gmail.com>
2021-10-22 12:16:57 -0500

    feature(git): add git aliases

--- plugins/git-extras/git-extras.sh
+++ plugins/git-extras/git-extras.sh
@@ -6,3 +6,11 @@ export GIT_EXTRAS_DIR="${FILES_USER_CONFIG}/git";
 [[ -f "${HOME}/.gitconfig" ]] || ln -sf "${FILES_PLUGIN_ROOT}/defaults.d/.gitconfig" "${HOME}/.gitconfig";
 
 files_linkdir "${FILES_PLUGIN_ROOT}/defaults.d" "${GIT_EXTRAS_DIR}" true;
+
+alias gs='git status';
+alias gm='git merge --ff-only';
+alias gpr='git pull --rebase';
+alias gmt='git mergetool';
+alias grc='git rebase --continue';
+alias gk='git fetch origin; git remote prune origin; gitk --all &';
+alias gl='git log --pretty=format:"%h %ar by %an: %s"';
