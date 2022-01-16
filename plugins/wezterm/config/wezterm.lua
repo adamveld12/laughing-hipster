60f929a41817509155701ad83609abd583a80b7e
Adam Veldhousen <adamveld12@gmail.com>
2022-01-16 16:48:53 -0600

    feat: add wezterm plugin

---
+++ plugins/wezterm/config/wezterm.lua
@@ -0,0 +1,6 @@
+local wez = require 'wezterm';
+
+return {
+    font = wez.font('Fira Code'),
+    default_prog = {"/bin/bash", "--login"}
+}
---
+++ plugins/wezterm/wezterm.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+WEZTERM_VERSION=""
+WEZTERM_INSTALL_DIR="${HOME}/bin"
+
+
+wezterm_install() {
+    yay -Sy wezterm-git
+}
+
